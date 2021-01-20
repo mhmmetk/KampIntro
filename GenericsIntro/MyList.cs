054dabf08d4bfcfb3b2bd9b0773952b086689136
Muhammet Kılbacak <kilbacak@hotmail.com>
2021-01-21 00:22:17 +0300

    20 ocak ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{//T =string,int tipi
+    class MyList<T>
+    {
+        T[] items;
+        //constructer
+        public MyList()
+        {
+            items = newT[0];
+        }
+        public void Add(T item)
+        {//tempArray geçici dizi
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{//snippet
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }//ürün ismi
+        public double UnitPrice { get; set; }//ürünün birim fiyatı
+        public int UnitsInStock { get; set; }//ürün stok adedi
+        //crud-create read update delete
+
+
+    }
+}
